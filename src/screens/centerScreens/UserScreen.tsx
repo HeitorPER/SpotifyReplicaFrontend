8ca6c0802e4ec4ef33079f02419c254153091539
HeitorPER <heitorgiometti@gmail.com>
2026-06-24 17:06:47 -0300

    fix(UserScreen):Implementa todas os elementos previstos na user screen

--- src/screens/centerScreens/UserScreen.tsx
+++ src/screens/centerScreens/UserScreen.tsx
@@ -1,8 +1,11 @@
 import { ArtistsCardRounded } from "../../components/ArtistsCardRounded";
 import { ImagePlaceholder } from "../../components/ImagePlaceholder";
+import { MusicCard } from "../../components/MusicCard";
+import { PlaylistSquareCard } from "../../components/playlistCards/playlistSquareCard";
 import { Shelf } from "../../components/SelfModel";
-import { mockArtists } from "../../data/mockArtists";
+import { mockArtists, getArtistName } from "../../data/mockArtists";
 import {mockPlaylists} from "../../data/mockPlaylists"
+import { mockSongs } from "../../data/mockSongs"
 
 interface UserScreenProps{
     imageUrl?: string
@@ -30,13 +33,31 @@ export default function UserScreen({imageUrl, name}:UserScreenProps){
                     <h2 className="text-sm text-gray-400">{mockPlaylists.length} playlists publicas</h2>
                 </div>
             </div>
-            <div className="flex w-full">
-                <Shelf
-                label="Artistas mais tocados esse mes"
-                >
+            <div className="flex flex-col w-full py-6 px-5">
+                <Shelf label="Artistas mais tocados esse mes">
                     <div className="flex gap-x-3">
                         {mockArtists.slice(0, 4).map((artist) => (
                             <ArtistsCardRounded name={artist.artist_name}/>
+                        ))}
+                    </div>
+                </Shelf>
+                <div>
+                    <h2 className="text-lg text-white font-semibold">Músicas mais tocadas esse mês</h2>
+                    <div className="flex flex-col">
+                        {mockSongs.slice(0, 5).map((song) => (
+                            <MusicCard
+                                key={song.music_id}
+                                title={song.title}
+                                artist={getArtistName(song.artist)}
+                                explicit={song.explicit}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <Shelf label="Playlists publicas">
+                    <div className="flex gap-x-3">
+                        {mockPlaylists.slice(0,4).map((playlist) => (
+                            <PlaylistSquareCard name={playlist.name}/>
                         ))}
                     </div>
                 </Shelf>

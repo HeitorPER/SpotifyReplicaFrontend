7a95aeef6fda1302c2006142431351de309b2dba
HeitorPER <heitorgiometti@gmail.com>
2026-07-22 11:08:04 -0300

    fix(PlaylistList):corrige a posição do playlist list que agora aparece em todos os casos

--- src/components/musicCardOptions/MusicOptionsMenu.tsx
+++ src/components/musicCardOptions/MusicOptionsMenu.tsx
@@ -39,7 +39,7 @@ function PlaylistsButton({imageUrl, name, playlistId, musicId, onSelect}:Playlis
     return(
         <button 
         onClick={handleClick}
-        className={`flex items-center justify-start gap-2
+        className={`flex items-center justify-start gap-2 border border-[#1c1b1b]
         rounded-lg hover:bg-[#2D2D2D] cursor-pointer w-full`}>
             <div className="w-12 h-12 shrink-0 rounded overflow-hidden">
                 {imageUrl
@@ -59,8 +59,8 @@ function PlaylistsList({musicId, onClose}:{musicId: string; onClose: () => void}
     const {data:Playlists} = useFetch(() => userService.getPlaylists(), [])
 
     return(
-        <div className="scrollable absolute right-full top-0 mr-2 w-56 max-h-72 overflow-y-auto
-        bg-black rounded-lg shadow-lg p-2 flex flex-col gap-1 z-50">
+        <div className="scrollable absolute left-10 top-0 mr-2 w-56 max-h-72 overflow-y-auto
+        bg-[#282828] rounded-lg shadow-lg p-2 flex flex-col gap-1 z-50">
             {Playlists?.length
             ?(Playlists.map((playlists) =>(
                 <PlaylistsButton

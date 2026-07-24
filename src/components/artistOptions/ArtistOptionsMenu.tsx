9f4f93a12030d3f13d248f9c9ff5aab82f1e5b0c
HeitorPER <heitorgiometti@gmail.com>
2026-07-24 10:30:41 -0300

    fix(AddArtist):Corrige o menu do artista colocando opção de seguir

--- src/components/artistOptions/ArtistOptionsMenu.tsx
+++ src/components/artistOptions/ArtistOptionsMenu.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import { RiUserAddLine } from "react-icons/ri";
 import { TbPinFilled } from "react-icons/tb";
 
 
@@ -22,6 +23,12 @@ export function ArtistOptionsMenu({}: ArtistOptionsProps){
         className="flex bg-[#282828] flex-col 
         rounded-lg px-4 py-2 gap-y-2">
             
+            <button
+            className="flex items-center text-[#B3B3B3] hover:text-white
+            cursor-pointer gap-2">
+                <RiUserAddLine/>
+                    Seguir
+            </button>
             <button
             className="flex items-center text-[#B3B3B3] hover:text-white
             cursor-pointer gap-2">

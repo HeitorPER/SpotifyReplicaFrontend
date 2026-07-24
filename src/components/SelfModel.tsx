d23ca19b8cb4cc1a6220e4e231ebdd79997ff2f8
HeitorPER <heitorgiometti@gmail.com>
2026-07-24 10:06:58 -0300

    fix(ShelfScroll):Corrige o shelf para agora poder deslizar verticalmente

--- src/components/SelfModel.tsx
+++ src/components/SelfModel.tsx
@@ -5,9 +5,9 @@ interface ShelfProps {
 
 export function Shelf({ label, children }: ShelfProps) {
     return (
-        <div className="flex flex-col gap-y-2">
+        <div className="flex flex-col gap-y-2 w-full">
             <h2 className="text-white font-semibold text-lg">{label}</h2>
-            <div>{children}</div>
+            <div className="overflow-x-auto no-scrollbar">{children}</div>
         </div>
     )
 }
